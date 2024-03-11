9ed7c0c7cc392184bd1f95e377aaf6eaa08cc4d1
Damiano Cipriani <damiano.cipriani@suse.com>
2024-03-11 11:34:05 +0100

    module/raid: raid grow adding a new base bdev
    Implemented the functionality to add a new base bdev to an existing
    raid bdev, growing the size of the raid if needed.
    Actually only raid1 supports growing with a new base bdev.
    
    Signed-off-by: Damiano Cipriani <damiano.cipriani@suse.com>
    (cherry picked from commit 1d3115b584c50c16b7d0e0d91c48449f9622313e)

--- module/bdev/raid/bdev_raid.h
+++ module/bdev/raid/bdev_raid.h
@@ -21,6 +21,17 @@ enum raid_process_type {
 
 typedef void (*raid_bdev_action_cb)(void *ctx, int rc);
 
+enum raid_bdev_update_state {
+	/* No ongoing updates on raid base bdevs */
+	RAID_BDEV_UPDATE_NONE,
+
+	/* A base bdev removal is ongoing */
+	RAID_BDEV_UPDATE_REMOVING,
+
+	/* A base bdev growth is ongoing */
+	RAID_BDEV_UPDATE_GROWING,
+};
+
 /*
  * raid_base_bdev_info contains information for the base bdevs which are part of some
  * raid. This structure contains the per base bdev information. Whatever is
@@ -81,6 +92,9 @@ struct raid_base_bdev_info {
 
 	/* context of the callback */
 	void			*configure_cb_ctx;
+
+	/* Set to true to skip the start of the rebuild process when adding to an online raid */
+	bool			skip_rebuild;
 };
 
 struct raid_bdev_io;
@@ -223,6 +237,9 @@ struct raid_bdev {
 	/* Callback and context for raid_bdev destruction */
 	raid_bdev_action_cb		destroy_cb;
 	void				*destroy_cb_ctx;
+
+	/* A flag to indicate that an operation to add/remove a base bdev is in progress */
+	enum raid_bdev_update_state	base_bdev_updating;
 };
 
 #define RAID_FOR_EACH_BASE_BDEV(r, i) \
@@ -250,6 +267,8 @@ const char *raid_bdev_process_to_str(enum raid_process_type value);
 void raid_bdev_write_info_json(struct raid_bdev *raid_bdev, struct spdk_json_write_ctx *w);
 int raid_bdev_remove_base_bdev(struct spdk_bdev *base_bdev, raid_bdev_action_cb cb_fn,
 			       void *cb_ctx);
+int raid_bdev_grow_base_bdev(struct raid_bdev *raid_bdev, char *base_bdev_name,
+			     raid_bdev_action_cb cb_fn, void *cb_arg);
 
 /*
  * RAID module descriptor
@@ -324,6 +343,11 @@ struct raid_bdev_module {
 				      struct raid_bdev_io_channel *raid_ch);
 
 	TAILQ_ENTRY(raid_bdev_module) link;
+
+	/* Handle for module specific operations needed for raid grow */
+	bool (*channel_grow_base_bdev)(struct raid_bdev *raid_bdev,
+				       struct raid_bdev_io_channel *raid_ch);
+
 };
 
 void raid_bdev_module_list_add(struct raid_bdev_module *raid_module);
@@ -349,6 +373,7 @@ struct spdk_io_channel *raid_bdev_channel_get_base_channel(struct raid_bdev_io_c
 void *raid_bdev_channel_get_module_ctx(struct raid_bdev_io_channel *raid_ch);
 struct raid_base_bdev_info *raid_bdev_channel_get_base_info(struct raid_bdev_io_channel *raid_ch,
 		struct spdk_bdev *base_bdev);
+uint8_t raid_bdev_channel_get_num_channels(struct raid_bdev_io_channel *raid_ch);
 void raid_bdev_process_request_complete(struct raid_bdev_process_request *process_req, int status);
 void raid_bdev_io_init(struct raid_bdev_io *raid_io, struct raid_bdev_io_channel *raid_ch,
 		       enum spdk_bdev_io_type type, uint64_t offset_blocks,
