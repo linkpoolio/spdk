0eb4bb44ed037685adff1a9e1e99b203f9bd15f8
Damiano Cipriani <damiano.cipriani@suse.com>
2024-12-10 13:51:22 +0100

    blob: enable external xattr change in ro blob
    Now it is possible to set and to remove external xattrs also
    in read only blobs.
    
    Longhorn 8666
    
    Signed-off-by: Damiano Cipriani <damiano.cipriani@suse.com>
    (cherry picked from commit c35ca628e753c4730f8a752eb2942d109de45a03)

--- lib/blob/blobstore.c
+++ lib/blob/blobstore.c
@@ -9481,7 +9481,7 @@ blob_set_xattr(struct spdk_blob *blob, const char *name, const void *value,
 
 	blob_verify_md_op(blob);
 
-	if (blob->md_ro) {
+	if (blob->md_ro && internal) {
 		return -EPERM;
 	}
 
@@ -9558,7 +9558,7 @@ blob_remove_xattr(struct spdk_blob *blob, const char *name, bool internal)
 
 	blob_verify_md_op(blob);
 
-	if (blob->md_ro) {
+	if (blob->md_ro && internal) {
 		return -EPERM;
 	}
 	xattrs = internal ? &blob->xattrs_internal : &blob->xattrs;
